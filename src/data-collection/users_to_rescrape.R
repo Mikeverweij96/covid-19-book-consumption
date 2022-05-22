4336aca3a58784f071c16c6e9f2d1a0304a3a44a
Mike Verweij <m.r.j.a.verweij@tilburguniversity.edu>
2022-05-22 11:35:14 +0200

    add data analysis

--- src/data-collection/users_to_rescrape.R
+++ src/data-collection/users_to_rescrape.R
@@ -15,13 +15,8 @@ All_users<-fread("C:/Users/mikev/OneDrive - TU Eindhoven/UvT/3 & 4 - Thesis Mark
 
 All_users$Perc_scraped<-All_users$Nr_Books_scraped/All_users$Nr.Books.Read
 
-Users_not_enough<- All_users %>% filter(Perc_scraped < 0.99)
+Users_not_enough<- All_users %>% filter(Perc_scraped < 1)
 
-Users_not_enough$diff<-Users_not_enough$Nr.Books.Read-Users_not_enough$Nr_Books_scraped
-
-Users_not_enough <- Users_not_enough %>% filter(diff>10)
-
-Users_not_enough <- Users_not_enough %>% filter(Perc_scraped>0)
 
 
 write.csv2(Users_not_enough, file = "Users_re_scrape.csv", fileEncoding = "UTF-8")
